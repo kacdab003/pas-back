e6ea1428bbbcb0a56334613e91a6ad6a1d85ba52
Karol <lokarcqaz@gmail.com>
2020-11-25 19:38:15 +0100

    dotenv implementation
    Connecting to db with dotenv

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-const uri = "";
+const uri = process.env.ATLAS_URI;
 
 try {
   mongoose.connect(
