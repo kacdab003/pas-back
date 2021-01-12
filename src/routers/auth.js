1be717e13e9189af7b64a80984583f56bee971ec
Kacper Dąbrowski <kacdab003@utp.edu.pl>
2021-01-12 10:23:28 +0100

    BE-10 - Add validator to sign up

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,10 +1,16 @@
 const authController = require("../controllers/authController");
-
+const { body } = require("express-validator");
 const express = require("express");
 
 const router = express.Router();
-
-router.put("/signup", authController.signUp);
+const validators = [
+  body("login").isLength({ min: 1, max: 32 }),
+  body("name").isLength({ min: 1, max: 32 }),
+  body("surname").isLength({ min: 1, max: 32 }),
+  body("position").matches(/\b(?:ENGINEER|TECHNICIAN)\b/),
+  body("password").isLength({ min: 6 }),
+];
+router.put("/signup", validators, authController.signUp);
 router.post("/login", authController.login);
 
 module.exports = router;
